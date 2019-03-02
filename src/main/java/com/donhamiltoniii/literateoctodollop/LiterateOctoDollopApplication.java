e9050f542a0fd389fecac045ad5b1b104b22dc76
Don Hamilton III <donhamiltoniii@gmail.com>
2019-03-02 15:46:22 -0500

    Initial Project Setup

---
+++ src/main/java/com/donhamiltoniii/literateoctodollop/LiterateOctoDollopApplication.java
@@ -0,0 +1,13 @@
+package com.donhamiltoniii.literateoctodollop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterateOctoDollopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterateOctoDollopApplication.class, args);
+	}
+
+}
